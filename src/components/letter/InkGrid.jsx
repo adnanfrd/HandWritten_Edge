3de033b7a3a2aab3bcee2e95b49e1ea8d88f0e85
adnanfrd <adnanmadhar74@gmail.com>
2025-03-28 20:57:04 +0500

    Inkgrid updated

--- src/components/letter/InkGrid.jsx
+++ src/components/letter/InkGrid.jsx
@@ -5,15 +5,15 @@ import { motion } from "framer-motion";
 import gsap from "gsap";
 
 const items = [
-  { id: 1, src: "/i6.jpg", title: "Royal Blue", desc: "Command attention with bright elegance." },
-  { id: 2, src: "/i7.jpg", title: "Elegant Black", desc: "Timeless and always in vogue." },
-  { id: 3, src: "/i8.jpg", title: "Passionate Red", desc: "For strong statements that stand out." },
-  { id: 4, src: "/i4.jpg", title: "Graceful Green", desc: "Refreshing, natural, and serene." },
-  { id: 5, src: "/i5.jpg", title: "Whispering Walnut Brown", desc: "A subtle vintage touch." },
-  { id: 6, src: "/i6.jpg", title: "Majestic Midnight Blue", desc: "Depth and richness combined." },
-  { id: 7, src: "/i7.jpg", title: "Radiance Pink", desc: "A color that speaks of warmth." },
-  { id: 8, src: "/i8.jpg", title: "Signature Violet", desc: "The essence of creativity." },
-  { id: 9, src: "/i4.jpg", title: "Turquoise Temptation", desc: "Cool, inviting, and fresh." },
+  { id: 1, src: "/i6.jpg", title: "Royal Blue", desc: "Command attention with bright elegance.", color: "text-[#3D3A34]" },
+  { id: 2, src: "/i7.jpg", title: "Elegant Black", desc: "Timeless and always in vogue.", color: "text-blue-600" },
+  { id: 3, src: "/i8.jpg", title: "Passionate Red", desc: "For strong statements that stand out.", color: "text-[#185B7D]" },
+  { id: 4, src: "/i4.jpg", title: "Graceful Green", desc: "Refreshing, natural, and serene.", color: "text-blue-600" },
+  { id: 5, src: "/i5.jpg", title: "Whispering Walnut Brown", desc: "A subtle vintage touch.", color: "text-[#FD4E23]" },
+  { id: 6, src: "/i6.jpg", title: "Majestic Midnight Blue", desc: "Depth and richness combined.", color: "text-[#3D3933]" },
+  { id: 7, src: "/i7.jpg", title: "Radiance Pink", desc: "A color that speaks of warmth.", color: "text-blue-600" },
+  { id: 8, src: "/i8.jpg", title: "Signature Violet", desc: "The essence of creativity.", color: "text-[#1D2132]" },
+  { id: 9, src: "/i4.jpg", title: "Turquoise Temptation", desc: "Cool, inviting, and fresh.", color: "text-blue-500" },
 ];
 
 const InkGrid = () => {
@@ -54,7 +54,7 @@ const InkGrid = () => {
               height={150}
               className="rounded-md"
             />
-            <h2 className="text-lg font-semibold mt-2">{item.title}</h2>
+            <h2 className={`text-lg font-semibold mt-2 ${item.color}`}>{item.title}</h2>
             <p className="text-gray-600 text-center mt-1">{item.desc}</p>
           </motion.div>
         ))}
